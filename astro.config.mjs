a4797c21851e9a6a4834af5e11976d257c675526
Alex <alex@Alexs-Laptop.local>
2026-05-22 00:37:32 +0300

    fix: set trailingSlash to never to prevent 301 redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.plz-daten.de',
+  trailingSlash: 'never',
   integrations: [
     sitemap({
       serialize(item) {
